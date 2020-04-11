be42fb6a12d7d09893dc0a64375f3542910fe10d
dbeetoven <beetovenson@gmail.com>
2020-04-10 22:39:15 -0300

    postcategory service mapped.

--- src/routes/common.routes.js
+++ src/routes/common.routes.js
@@ -2,9 +2,11 @@ const express =require('express');
 const router = express.Router();
 const verifyAuth = require('../middlewares/verifyAuth');
 
-const {nationality,setNationalitties} = require('../controllers/nationalityController')
+const {nationality} = require('../controllers/nationalityController')
+const {postCategory} = require('../controllers/postCategoryController')
 
 router.get('/nationality',verifyAuth,nationality);
+router.get('/postCategory',verifyAuth,postCategory);
 
 
 module.exports=router;
