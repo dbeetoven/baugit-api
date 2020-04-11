3f1040fb24579921f0ec532ec8cdaa1bac6192f7
dbeetoven <beetovenson@gmail.com>
2020-04-11 01:01:19 -0300

    profile changed

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -2,14 +2,32 @@ const Post = require('../models/post.model');
 
 const create = async (req, res) => {
   try {
-    
-    const post = await new Post(req.body).save();
+    const post = Post(req.body);
+    await post.save();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+const update = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate({ id: req.body._id }, { $set: req.body }, { upsert: true });
+    if(!post) res.send(404).json({error:"post Not Foud"})
     res.status(200).json(post);
   } catch (error) {
     res.status(400).send(error);
   }
 };
 
+const remove = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete({ id: req.body._id });
+    if(!post) res.send(404).json({error:"post Not Foud"})
+    res.status(200).json({message:"post deleted"});
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 const findAll = async (req, res) => {
   try {
     const users = await Post.find()
@@ -23,5 +41,7 @@ const findAll = async (req, res) => {
 
 module.exports = {
   create,
-  findAll
+  findAll,
+  update,
+  delete
 };
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {create,findAll} = require('../controllers/postController');
+const {create,findAll,remove,update} = require('../controllers/postController');
 
 const verifyAuth = require('../middlewares/verifyAuth');
 
 router.post('/create', verifyAuth,create);
+router.delete('/:id', verifyAuth,remove);
+router.update('/:id', verifyAuth,update);
 router.get('/findAll', verifyAuth,findAll);
 
 module.exports = router;
