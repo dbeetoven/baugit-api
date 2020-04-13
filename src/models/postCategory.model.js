0e9144c6d2a24285aea354994ecc28b82e4879d9
dbeetoven <beetovenson@gmail.com>
2020-04-13 19:40:57 -0300

    unexpected token resolved

--- src/models/postCategory.model.js
+++ src/models/postCategory.model.js
@@ -2,9 +2,9 @@ const  mongoose = require('mongoose');
 const  Schema  = mongoose.Schema;
 
 const postCategorySchema = new Schema({
-    title:{type:String, required:true, maxlength:64}
+    title:{type:String, required:true, maxlength:64},
     name: {type : String, required : true,maxlength:64},
-    permissionLevel:{type:Number,required:true}
+    permissionLevel:{type:Number,required:true},
     created_at: { type: Date, default: Date.now }
 });
 module.exports =  mongoose.model('postCatorgies', postCategorySchema);
